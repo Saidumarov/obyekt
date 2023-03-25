3c5654397e31341522aaaf27e3f033fac7de1e36
Saidumarov <saidumarovjafarxon@gmail.com>
2023-03-25 15:24:04 +0500

    first commit

---
+++ book.js
@@ -0,0 +1,56 @@
+
+// book masalasi
+let book = {
+    name : "Xakkerlar",
+    muallif : "Ja'farxon Saidumarov",
+    Sana:` 25.03.2023`,
+    sahifa : "500 pages"
+
+}
+console.log(book);
+
+//kompyuter masalasi
+
+let kompyuter ={
+    name:"hp Victus",
+    ram :16 +" GB"+" DDR" + 4,
+    xotira : 512+" GB" + "  SSD",
+    ekran :15.6+" FHD",
+    protsessor: "RYZEN " + 5000,
+   " vedio karta " : "NVIDIA  " + 4 +" GB" ,
+   sistema :"Wendows " + 10,
+   
+ 
+}
+console.log(kompyuter);
+
+// person nomli masala
+
+let person = {
+    name :"Ja'farxon",
+    lastname :"Saidumarov",
+    profession :"Dasturchi",
+    age :17,
+    sex :"erkag",
+    address :"Ishtixon Shihlar mahallasi",
+    mobile :+998990580714,
+
+    father :{
+        name :"Mardonxon",
+        lastname :"Sadirdinov",
+        profession : "Beznis",
+        birth : `26.08 .1982`,
+
+    },
+
+    mother :{
+        name :"Dilobar",
+        lastname :"Rahmatova",
+        profession :"Uybekasi",
+        birth :`06.09.1984`
+    }
+}
+console.log(person.father);
+
+
+
